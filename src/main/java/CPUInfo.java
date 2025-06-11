98e9ba3aa11d03e398d49b0949dd487743570dff
Doan Quang Thanh <respectthanh@gmail.com>
2025-06-11 15:55:04 +0700

    refactor: rm unnecessary files

--- src/main/java/CPUInfo.java
+++
@@ -1,9 +0,0 @@
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-
-public class CPUInfo {
-    public static void main(String[] args) {
-        CentralProcessor cpu = new SystemInfo().getHardware().getProcessor();
-        System.out.println("Tên CPU: " + cpu.getProcessorIdentifier().getName());
-    }
-}
